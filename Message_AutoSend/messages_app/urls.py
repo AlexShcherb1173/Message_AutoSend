bb425b692cbf9c10cdeb81b17462a5a0fee206d3
Alex Shcherbyna <Alex_181173@list.ru>
2025-10-28 14:51:59 +0300

    step2

--- Message_AutoSend/messages_app/urls.py
+++ Message_AutoSend/messages_app/urls.py
@@ -10,9 +10,6 @@ urlpatterns = [
     path("<int:pk>/", views.MessageDetailView.as_view(), name="message_detail"),
     path("<int:pk>/edit/", views.MessageUpdateView.as_view(), name="message_update"),
     path("<int:pk>/delete/", views.MessageDeleteView.as_view(), name="message_delete"),
-    path("admin/", admin.site.urls),
-    path("clients/", include("clients.urls", namespace="clients")),
-    path("messages/", include("messages_app.urls", namespace="messages_app")),
 ]
 
 
